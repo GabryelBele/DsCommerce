4a767ddcd52a41f9798c718c33f4542261f6a212
Gabryel Bele <belegabryel19@gmail.com>
2023-12-23 13:15:37 -0300

    GET User Logged

---
+++ src/main/java/com/devsuperior/DScommerce/controllers/UserController.java
@@ -0,0 +1,26 @@
+package com.devsuperior.DScommerce.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.DScommerce.dto.UserDTO;
+import com.devsuperior.DScommerce.services.UserService;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+	@Autowired
+	private UserService service;
+
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_CLIENT')")
+	@GetMapping("/me")
+	public ResponseEntity<UserDTO> getMe() {
+		UserDTO dto = service.getMe();
+		return ResponseEntity.ok(dto);
+	}
+}
--- src/main/java/com/devsuperior/DScommerce/services/UserService.java
+++ src/main/java/com/devsuperior/DScommerce/services/UserService.java
@@ -3,13 +3,18 @@ package com.devsuperior.DScommerce.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.DScommerce.domain.Role;
 import com.devsuperior.DScommerce.domain.User;
+import com.devsuperior.DScommerce.dto.UserDTO;
 import com.devsuperior.DScommerce.projections.UserDetailsProjection;
 import com.devsuperior.DScommerce.repositories.UserRepository;
 
@@ -21,19 +26,36 @@ public class UserService implements UserDetailsService {
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		
+
 		List<UserDetailsProjection> result = repository.searchUserAndRolesByEmail(username);
 		if (result.size() == 0) {
 			throw new UsernameNotFoundException("Email not found");
 		}
-		
+
 		User user = new User();
 		user.setEmail(result.get(0).getUsername());
 		user.setPassword(result.get(0).getPassword());
 		for (UserDetailsProjection projection : result) {
 			user.addRole(new Role(projection.getRoleId(), projection.getAuthority()));
 		}
-		
+
 		return user;
+	}
+
+	protected User authenticated() {
+		try {
+			Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+			Jwt jwtPrincipal = (Jwt) authentication.getPrincipal();
+			String username = jwtPrincipal.getClaim("username");
+			return repository.findByEmail(username).get();
+		} catch (Exception e) {
+			throw new UsernameNotFoundException("User not found");
+		}
+	}
+	
+	@Transactional(readOnly = true)
+	public UserDTO getMe() {
+		User user = authenticated();
+		return new UserDTO(user);
 	}
 }
